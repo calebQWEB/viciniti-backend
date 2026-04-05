f09bf02d8d874e0be57f3e7bbe7994950387c157
RexCaleb89 <c.ochai@rexinsure.com>
2026-04-05 12:22:40 +0100

    Add Vercel URL to CORS origins

--- app/main.py
+++ app/main.py
@@ -14,6 +14,8 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://viciniti.vercel.app",
+    "https://www.viciniti.com",
 ]
 
 app.add_middleware(
