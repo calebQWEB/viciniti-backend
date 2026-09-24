aca2502c3e831e046c2b46556896e87dcd8afc74
RexCaleb89 <c.ochai@rexinsure.com>
2026-09-24 13:44:24 +0100

    Backend update.

--- alembic/env.py
+++ alembic/env.py
@@ -24,7 +24,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database import Base
-from app.models import User, Listing, Service, Booking, Order, Message, Notification, Transaction, PasswordResetToken, BankAccount, Review
+from app.models import User, Listing, Service, Booking, Order, Message, Notification, Transaction, PasswordResetToken, BankAccount, Review, Category
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
